1cf2f411e57d7f1b0fe226ec85c5bdb9bb8bdd0c
Vineet Tiruvadi <virati@gmail.com>
2022-02-08 10:28:08 -0800

    got the simulator working again, now with exposure for stim freq

--- analysis/simulated/example_simulation.py
+++ analysis/simulated/example_simulation.py
@@ -1,5 +1,4 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+#%%
 """
 Created on Mon Dec 16 18:07:46 2019
 
@@ -7,22 +6,20 @@ Created on Mon Dec 16 18:07:46 2019
 Splitting our simulation of the dLFP into separate script
 """
 
-import DBSpace.dLFP.diff_model as diff_model
-from DBSpace.dLFP.diff_model import sim_diff, sim_amp
+import dbspace.signal.dLFP.diff_model as diff_model
+from dbspace.signal.dLFP.diff_model import sim_diff, sim_amp
+import matplotlib.pyplot as plt
 
-print('dLFP Simulation for Mismatch Compression - Script')
-diff_run = sim_diff(Ad=200,wform='moresine4',clock=True,stim_v=4)
-#diff_run.set_brain()
-#diff_run.set_stim(wform='ipg')
+plt.rcParams["figure.figsize"] = (20, 10)
 
-amp_run = sim_amp(diff_run,family='tanh',noise=1e-6,sig_amp_gain=100,pre_amp_gain=10)
+#%%
+diff_run = sim_diff(Ad=200, wform="moresine4", clock=True, stim_v=4, stim_freq=130)
+amp_run = sim_amp(diff_run, family="tanh", noise=1e-6, sig_amp_gain=10, pre_amp_gain=10)
+Z1, Z3 = 1200, 1300
 
-#diff_run.plot_V_out(1000,1200)
-#diff_out = diff_run.V_out(1000,1100)['sim_1']
-Z1 = 1200
-Z3 = 1300
-
-amp_run.simulate(Z1,Z3,use_windowing='blackmanharris')
+amp_run.simulate(Z1, Z3, use_windowing="blackmanharris")
 amp_run.plot_time_dom()
 amp_run.plot_freq_dom()
 amp_run.plot_tf_dom()
+
+plt.show()
