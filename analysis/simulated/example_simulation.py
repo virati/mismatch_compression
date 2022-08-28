d0d5f13ec4dabba3922387dc21bf3b6b17d5588f
Vineet Tiruvadi <virati@gmail.com>
2022-08-28 01:42:20 +0000

    checking order of sampling and filtering

--- analysis/simulated/example_simulation.py
+++ analysis/simulated/example_simulation.py
@@ -6,8 +6,8 @@ Created on Mon Dec 16 18:07:46 2019
 Splitting our simulation of the dLFP into separate script
 """
 
-import dbspace.signal.dLFP.diff_model as diff_model
-from dbspace.signal.dLFP.diff_model import sim_diff, sim_amp
+import dbspace.signal.dLFP.diff_amp_model as diff_model
+import dbspace.signal.dLFP.sig_amp_model as sig_amp
 import matplotlib.pyplot as plt
 
 plt.rcParams["figure.figsize"] = (20, 10)
@@ -19,7 +19,7 @@ plt.rcParams["figure.figsize"] = (20, 10)
 
 #%%
 # Parameter set
-Z1, Z3 = 1200, 1300
+Z1, Z3 = 1300, 1300
 stimulation_frequency = 130
 amp_model = "tanh"
 
@@ -30,16 +30,24 @@ amp_model = "tanh"
 
 
 #%%
-diff_run = sim_diff(
-    Ad=200, wform="moresine4", clock=True, stim_v=4, stim_freq=stimulation_frequency
+diff_run = diff_model.sim_diff(
+    Ad=10,
+    wform="moresine4",
+    clock=True,
+    stim_v=4,
+    stim_freq=stimulation_frequency,
+    full_Fs=10000,
 )
-amp_run = sim_amp(
-    diff_run, family=amp_model, noise=1e-6, sig_amp_gain=10, pre_amp_gain=5
+amp_run = sig_amp.sim_amp(
+    diff_run, family=amp_model, noise=1e-6, sig_amp_gain=10, pre_amp_gain=0.1
 )
 
-amp_run.simulate(Z1, Z3, use_windowing="blackmanharris")
+amp_run.simulate(Z1, Z3)
+amp_run.plot_simulation()
 amp_run.plot_time_dom()
 amp_run.plot_freq_dom()
 amp_run.plot_tf_dom()
 
 plt.show()
+
+# %%
